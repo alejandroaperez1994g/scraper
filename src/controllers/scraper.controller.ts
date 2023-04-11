4175dde101215cae0c61daa913f429fe74eb626b
Alejandro <alejandroaperez1994g@gmail.com>
2023-04-11 20:23:11 +0200

    :recycle: organized the routes and controllers

--- src/controllers/scraper.controller.ts
+++ src/controllers/scraper.controller.ts
@@ -2,9 +2,9 @@ import {Request, Response} from "express"
 import axios from "axios"
 import cheerio from "cheerio"
 
-import {PorsheType} from "../models/porshe.model"
-import {getModelsData} from "../utils/scraper.utils";
-
+import {PorsheType} from "../interfaces/porsche.interface"
+import {getModelsData, scraperWithPuppeteer} from "../utils/scraper.utils";
+import porscheModel from "../models/porsche.model";
 
 export const getPorsheModels = async (req: Request, res: Response) => {
     const {url} = req.body
@@ -40,15 +40,50 @@ export const getPorshes = async (req: Request, res: Response) => {
     if (!url) res.status(400)
 
     try {
-        // const allModelsData = await scraperWithPuppeteer(url)
+        const allModelsData = await scraperWithPuppeteer(url)
 
-
-        const fake = await getModelsData('https://www.porsche.com/usa/models/718/718-models/718-cayman/')
-        console.log(fake)
-
-        res.status(200).send("fake")
+        res.status(200).send(allModelsData)
     } catch (e) {
         console.log(e)
         res.status(500).send({msg: e})
     }
+}
+export const getPorshesWithData = async (req: Request, res: Response) => {
+    const {url} = req.body
+
+    if (!url) res.status(400)
+
+    try {
+        const allModelsData = await scraperWithPuppeteer(url)
+
+        const newData = await Promise.all(
+            allModelsData.slice(0, 11).map((model) => {
+                // if(model.model_series === "718 Models") {
+                return getModelsData(model.link_url).then(data => data)
+                // }
+            })
+        );
+
+        const combinedData = allModelsData.slice(0, newData.length).map((model, index) => {
+            return {...model, tech: newData[index]}
+        })
+
+        // const fake = await getModelsData('https://www.porsche.com/usa/models/718/718-models/718-cayman-style-edition/')
+
+        res.status(200).send(combinedData)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send({msg: e})
+    }
+}
+
+export const getAllPorscheModels = async (req, res) => {
+    try {
+        const models = await porscheModel.find().lean().exec()
+        res.status(200).send({models})
+    } catch (e) {
+        console.log(e)
+        res.status(500).send({msg: e})
+    }
+
 }
--- src/routes/scraper.routes.ts
+++ src/routes/scraper.routes.ts
@@ -1,15 +1,11 @@
 import {Router} from "express";
-import {getPorsheModels, getPorshes} from "../controllers/scraper.controller";
+import {getAllPorscheModels} from "../controllers/scraper.controller";
 
 
 const scraperRouter = Router()
 
 
-scraperRouter.get("/", (req, res) => {
-    res.send("Todo OK")
-})
-    .post("/scraper", getPorsheModels)
-    .post("/scraper2", getPorshes)
+scraperRouter.get("/", getAllPorscheModels)
 
 
 export default scraperRouter
