8280fc97edde92d2d49e851e6314e0fd0b38e2c2
Alejandro <alejandroaperez1994g@gmail.com>
2023-05-16 18:40:50 +0200

    feat: :recycle: updated controllers with prisma

--- src/controllers/models.controller.ts
+++ src/controllers/models.controller.ts
@@ -1,9 +1,8 @@
-import porscheModel from "../models/porsche.model";
-
+import {prisma} from "../db";
 
 export const getAllPorscheModels = async (req, res) => {
     try {
-        const models = await porscheModel.find().lean().exec()
+        const models = await prisma.porsche.findMany()
         res.status(200).send({models})
     } catch (e) {
         console.log(e)
@@ -15,12 +14,16 @@ export const getAllPorscheModels = async (req, res) => {
 
 export const getPorscheModelById = async (req, res) => {
     const {id} = req.params
-
     if (!id) {
         res.status(400).send({msg: "Id is required"})
     }
+    const model = await prisma.porsche.findUnique({
+        where: {
+            id: id
+        }
+    })
+    // console.dir({model},{depth: Infinity})
     try {
-        const model = await porscheModel.findById(id).lean().exec()
         res.status(200).send({model})
     } catch (e) {
         res.status(500).send({msg: e})
@@ -28,3 +31,5 @@ export const getPorscheModelById = async (req, res) => {
 
 
 }
+
+
